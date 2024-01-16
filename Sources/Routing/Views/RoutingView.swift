804212d322bd84e37544f3126769d2d6abeb31e1
James Sedlacek <Jsedlacekjr@gmail.com>
2024-01-16 14:06:05 -0500

    Removed router from closure

--- Sources/Routing/Views/RoutingView.swift
+++ Sources/Routing/Views/RoutingView.swift
@@ -8,16 +8,16 @@ import SwiftUI
 
 public struct RoutingView<RootView: View, Routes: Routable>: View {
     @ObservedObject private var router: Router<Routes>
-    private let rootView: (Router<Routes>) -> RootView
+    private let rootView: () -> RootView
     
-    public init(_ router: Router<Routes>, @ViewBuilder rootView: @escaping (Router<Routes>) -> RootView) {
+    public init(_ router: Router<Routes>, @ViewBuilder rootView: @escaping () -> RootView) {
         self.router = router
         self.rootView = rootView
     }
 
     public var body: some View {
         NavigationStack(path: $router.stack) {
-            rootView(router)
+            rootView()
                 .navigationDestination(for: Router<Routes>.Destination.self) { route in
                     route.body.environmentObject(router)
                 }
